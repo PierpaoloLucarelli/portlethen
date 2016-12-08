3bb661d7c32cc4468107352ab8bbd2c30cac8ca3
jmudri <janos.mudri@gmail.com>
2016-12-08 15:05:06 +0000

    db test2

--- editclub.php
+++ editclub.php
@@ -1,24 +1,24 @@
 <?php
  $clubId = $_GET["clubId"];
 
- if(isset($_POST['update']))
+ if(isset($_POST['update'])) {
 
- $name = $_POST['name'];
- $category = $_POST['category'];
- $description = $_POST['description'];
- $clubImage = $_POST['clubImage'];
- $mobilePhone = $_POST['mobilePhone'];
- $housePhone = $_POST['housePhone'];
- $address = $_POST['address'];
- $city = $_POST['city'];
- $postcode = $_POST['postcode'];
- $country = $_POST['country'];
+     $name = $_POST['name'];
+     $category = $_POST['category'];
+     $description = $_POST['description'];
+     $clubImage = $_POST['clubImage'];
+     $mobilePhone = $_POST['mobilePhone'];
+     $housePhone = $_POST['housePhone'];
+     $address = $_POST['address'];
+     $city = $_POST['city'];
+     $postcode = $_POST['postcode'];
+     $country = $_POST['country'];
 
- include 'dbConfig.php';
+     include 'dbConfig.php';
 
- $sql = $db->query("UPDATE clubs SET clubName = '$name', clubDesc = '$description', clubImage = '$clubImage' WHERE clubId =".$clubId);
+     $sql = $db->query("UPDATE clubs SET clubName = '$name', clubDesc = '$description', clubImage = '$clubImage' WHERE clubId =" . $clubId);
 
-
+ }
 ?>
 <!DOCTYPE html>
 <!--[if IE 8]> <html lang="en" class="ie8"> <![endif]-->
