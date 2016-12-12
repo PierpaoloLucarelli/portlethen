c506986c5445bd95e795236cc21131d827d7e817
Thanos Sivridis <Jehutty@outlook.com>
2016-12-12 21:46:39 +0000

    Added footer to all pages dynamically

--- index.php
+++ index.php
@@ -28,19 +28,7 @@
 </head>
 <body>
 <div id="body-bg">
-    <!-- Phone/Email -->
-    <div id="pre-header" class="background-gray-lighter">
-        <div class="container no-padding">
-            <div class="row hidden-xs">
-                <div class="col-sm-6 padding-vert-5">
-                    <strong>Phone:</strong>&nbsp;1-800-123-4567
-                </div>
-                <div class="col-sm-6 text-right padding-vert-5">
-                    <strong>Email:</strong>&nbsp;info@joomla51.com
-                </div>
-            </div>
-        </div>
-    </div>
+
     <!-- End Phone/Email -->
     <!-- Header -->
     <div id="header">
@@ -244,74 +232,9 @@
     </div>
     <!-- End Portfolio -->
     <!-- === END CONTENT === -->
-    <!-- === BEGIN FOOTER === -->
-    <div id="base">
-        <div class="container bottom-border padding-vert-30">
-            <div class="row">
-                <!-- Disclaimer -->
-                <div class="col-md-4">
-                    <h3 class="class margin-bottom-10">Disclaimer</h3>
-                    <p>All stock images on this template demo are for presentation purposes only, intended to represent a live site and are not included with the template or in any of the Joomla51 club membership plans.</p>
-                    <p>Most of the images used here are available from
-                        <a href="http://www.shutterstock.com/" target="_blank">shutterstock.com</a>. Links are provided if you wish to purchase them from their copyright owners.</p>
-                </div>
-                <!-- End Disclaimer -->
-                <!-- Sample Menu -->
-                <div class="col-md-4 margin-bottom-20">
 
-                </div>
-                <!-- End Sample Menu -->
-                <!-- Contact Details -->
-                <div class="col-md-4 margin-bottom-20">
-                    <h3 class="margin-bottom-10">Contact Details</h3>
-                    <p>
-                        <span class="fa-phone">Telephone:</span>1-800-123-4567
-                        <br>
-                        <span class="fa-envelope">Email:</span>
-                        <a href="mailto:info@example.com">info@example.com</a>
-                        <br>
-                        <span class="fa-link">Website:</span>
-                        <a href="http://www.example.com">www.example.com</a>
-                    </p>
-                    <p>The Dunes, Top Road,
-                        <br>Strandhill,
-                        <br>Co. Sligo,
-                        <br>Ireland</p>
-                </div>
-                <!-- End Contact Details -->
-            </div>
-        </div>
-    </div>
-    <!-- Footer -->
-    <div id="footer" class="background-grey">
-        <div class="container">
-            <div class="row">
-                <!-- Footer Menu -->
-                <div id="footermenu" class="col-md-8">
-                    <ul class="social-icons pull-right">
-                        <li class="social-rss">
-                            <a href="#" target="_blank" title="RSS"></a>
-                        </li>
-                        <li class="social-twitter">
-                            <a href="#" target="_blank" title="Twitter"></a>
-                        </li>
-                        <li class="social-facebook">
-                            <a href="#" target="_blank" title="Facebook"></a>
-                        </li>
-                        <li class="social-googleplus">
-                            <a href="#" target="_blank" title="Google+"></a>
-                        </li>
-                    </ul>
-                </div>
-                <!-- End Footer Menu -->
-                <!-- Copyright -->
-                <div id="copyright" class="col-md-4">
-                    <p class="pull-right">Copyright info 2016 $copy;</p>
-                </div>
-                <!-- End Copyright -->
-            </div>
-        </div>
-    </div>
+    <!-- === BEGIN FOOTER === -->
+    <?php  include "./includes/footer.php" ?>
     <!-- End Footer -->
     <!-- JS -->
     <script type="text/javascript" src="assets/js/jquery.min.js" type="text/javascript"></script>
