4c89e50cc5dad273de424f7a1cc9b450b6a550e0
Thanos Sivridis <Jehutty@outlook.com>
2016-12-12 21:27:44 +0000

    Fixed footer menu appearance bug added filter on loginprocess to distinguish users with role id instead of user id

--- loginProcess.php
+++ loginProcess.php
@@ -1,11 +1,13 @@
 <?php
 include "dbConfig.php";
-
 $username = $_POST['username'];
 $password = $_POST['password'];
+$stmt = $db->prepare("SELECT * from users WHERE username LIKE ? AND password LIKE ? LIMIT 1");
+$stmt->bind_param('ss', $username, $password);
 
-$sql = "SELECT * from users WHERE username LIKE '{$username}' AND password LIKE '{$password}' LIMIT 1";
-$result = $db->query($sql);
+$stmt->execute();
+$result = $stmt->get_result();
+
 $row = $result->fetch_assoc();
 if (!$result->num_rows == 1) {
     echo "<p>Invalid username/password combination</p>";
