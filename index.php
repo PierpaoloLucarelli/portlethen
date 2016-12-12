175fb2be84e31d4371e41183afe1b60c323a84c3
Thanos Sivridis <Jehutty@outlook.com>
2016-12-12 23:07:39 +0000

    Add files via upload

---
+++ index.php
@@ -0,0 +1,68 @@
+<!-- === BEGIN HEADER === -->
+<!DOCTYPE html>
+<!--[if IE 8]> <html lang="en" class="ie8"> <![endif]-->
+<!--[if IE 9]> <html lang="en" class="ie9"> <![endif]-->
+<!--[if !IE]><!-->
+<html lang="en">
+<!--<![endif]-->
+<head>
+    <!-- Title -->
+    <title>Habitat - A Professional Bootstrap Template</title>
+    <!-- Meta -->
+    <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+    <!-- Favicon -->
+    <link href="favicon.ico" rel="shortcut icon">
+    <!-- Bootstrap Core CSS -->
+    <link rel="stylesheet" href="assets/css/bootstrap.css" rel="stylesheet">
+    <!-- Template CSS -->
+    <link rel="stylesheet" href="assets/css/animate.css" rel="stylesheet">
+    <link rel="stylesheet" href="assets/css/font-awesome.css" rel="stylesheet">
+    <link rel="stylesheet" href="assets/css/nexus.css" rel="stylesheet">
+    <link rel="stylesheet" href="assets/css/responsive.css" rel="stylesheet">
+    <link rel="stylesheet" href="assets/css/custom.css" rel="stylesheet">
+    <!-- Google Fonts-->
+    <link href="http://fonts.googleapis.com/css?family=Roboto+Condensed:400,300" rel="stylesheet" type="text/css">
+</head>
+<body>
+<div id="body-bg">
+
+    <!-- End Phone/Email -->
+    <!-- Header -->
+    <?php include "./includes/header.php"?>
+    <!-- End Header -->
+    <!-- Top Menu -->
+    <?php  include "./includes/nav.php"?>
+    <!-- End Top Menu -->
+    <!-- === END HEADER === -->
+    <!-- begin content -->
+    <?php include "./includes/index_content.php"?>
+    <!-- end content -->
+</div>
+
+
+    <!-- === BEGIN FOOTER === -->
+    <?php  include "./includes/footer.php" ?>
+    <!-- End Footer -->
+    <!-- JS -->
+    <script type="text/javascript" src="assets/js/jquery.min.js" type="text/javascript"></script>
+    <script type="text/javascript" src="assets/js/bootstrap.min.js" type="text/javascript"></script>
+    <script type="text/javascript" src="assets/js/scripts.js"></script>
+    <!-- Isotope - Portfolio Sorting -->
+    <script type="text/javascript" src="assets/js/jquery.isotope.js" type="text/javascript"></script>
+    <!-- Mobile Menu - Slicknav -->
+    <script type="text/javascript" src="assets/js/jquery.slicknav.js" type="text/javascript"></script>
+    <!-- Animate on Scroll-->
+    <script type="text/javascript" src="assets/js/jquery.visible.js" charset="utf-8"></script>
+    <!-- Sticky Div -->
+    <script type="text/javascript" src="assets/js/jquery.sticky.js" charset="utf-8"></script>
+    <!-- Slimbox2-->
+    <script type="text/javascript" src="assets/js/slimbox2.js" charset="utf-8"></script>
+    <!-- Modernizr -->
+    <script src="assets/js/modernizr.custom.js" type="text/javascript"></script>
+    <!-- End JS -->
+</body>
+</html>
+<!-- === END FOOTER === -->
