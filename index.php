caf8c31b56c95fb5213b319f3580f3eb18e17c66
Thanos Sivridis <Jehutty@outlook.com>
2016-12-12 18:19:18 +0000

    Various front end changes.
    Changed navigation bar to a fully width one
    Changed color strata of the site
    Fixed slider inability of showing all images.

--- index.php
+++ index.php
@@ -28,19 +28,6 @@
     </head>
     <body>
         <div id="body-bg">
-            <!-- Phone/Email -->
-            <div id="pre-header" class="background-gray-lighter">
-                <div class="container no-padding">
-                    <div class="row hidden-xs">
-                        <div class="col-sm-6 padding-vert-5">
-                            <strong>Phone:</strong>&nbsp;1-800-123-4567
-                        </div>
-                        <div class="col-sm-6 text-right padding-vert-5">
-                            <strong>Email:</strong>&nbsp;info@joomla51.com
-                        </div>
-                    </div>
-                </div>
-            </div>
             <!-- End Phone/Email -->
             <!-- Header -->
             <div id="header">
@@ -72,6 +59,7 @@
                                 <li data-target="#carousel-example" data-slide-to="0" class="active"></li>
                                 <li data-target="#carousel-example" data-slide-to="1"></li>
                                 <li data-target="#carousel-example" data-slide-to="2"></li>
+                                <li data-target="#carousel-example" data-slide-to="3"></li>
                             </ol>
                             <div class="clearfix"></div>
                             <!-- End Carousel Indicators -->
